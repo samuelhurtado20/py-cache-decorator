dbd8a44144cb0f2bde56be8b355418b18b0a9656
Samuel Hurtado <samuelhurtado20@hotmail.com>
2026-03-06 15:38:44 -0500

    Add caching mechanism to cache decorator
    Implement caching functionality in the cache decorator.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from typing import Callable
 
 
-def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+def cache(func: Callable):
+    stored_results = {}
+    def wrapper(*args):
+        if args in stored_results:
+            print("Getting from cache")
+            return stored_results[args]
+        print("Calculating new result")
+        result = func(*args)
+        stored_results[args] = result
+        return result
+    return wrapper
